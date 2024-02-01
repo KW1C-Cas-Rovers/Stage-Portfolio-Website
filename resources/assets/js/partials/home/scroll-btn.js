80216b1d74e418959db60434a2d447cab9a9b13e
Cas <Cas@Casis.com>
2024-02-01 09:21:05 +0100

    Added scroll btn  to intro section homepage

--- resources/assets/js/partials/home/scroll-btn.js
+++ resources/assets/js/partials/home/scroll-btn.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    scroll_btn = $('.scroll-btn');
-    html_and_body = $('html, body');
-    about_me_section = $('#about-me');
+    let scroll_btn = $('.scroll-btn');
+    let html_and_body = $('html, body');
+    let about_me_section = $('#about-me');
 
     scroll_btn.click(function () {
         html_and_body.animate({
