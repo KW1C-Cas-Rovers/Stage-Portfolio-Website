c43603a032180d12dedea01ffdfd8f078eb29ef1
Cas <Cas@Casis.com>
2024-02-02 11:18:49 +0100

    Updated the footer and added the copyright and some info buttons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,44 +1,47 @@
 <footer class="footer">
     <div class="footer-wrapper">
         <div class="container">
-            <div class="row">
+            <div class="row gap-2 gap-md-0">
+                <h2 class="text-center">Contact.</h2>
                 <div class="col-md-6">
                     <div class="footer-message">
                         <h3 class="footer-title">Drop me a message.</h3>
                         <p class="footer-description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt leo sit amet tempor
-                            ullamcorper. Suspendisse felis leo, rutrum nec mattis at, vehicula vehicula metus. Nullam
-                            porttitor dolor vel elementum elementum. Morbi quis congue sem. Nam euismod eros auctor sapien
-                            lacinia imperdiet. Vestibulum mollis quam urna, at ornare nisi efficitur non. Vestibulum dictum
-                            nibh sed tincidunt mattis.
+                            {{ __('Als u vragen heeft over wat ik doe en/of wie ik ben, kunt u mij een bericht sturen of
+                            contact met mij opnemen.') }}
                         </p>
                     </div>
                     <div class="footer-contact-info">
                         <div class="contact-info-wrapper">
                             <div class="tel">
                                 <div class="image-wrapper">
-                                    <img class="contact-img" src="{{ mix('assets/media/images/icons/telephone.svg')  }}" alt="Telephone icon">
+                                    <img class="contact-img" src="{{ mix('assets/media/images/icons/telephone.svg')  }}"
+                                         alt="Telephone icon">
                                 </div>
                                 <p class="contact-info">+31 6 24737066</p>
                             </div>
                             <div class="email">
                                 <div class="image-wrapper">
-                                    <img class="contact-img" src="{{ mix('assets/media/images/icons/envelope-at.svg')  }}" alt="Telephone icon">
+                                    <img class="contact-img"
+                                         src="{{ mix('assets/media/images/icons/envelope-at.svg')  }}"
+                                         alt="Telephone icon">
                                 </div>
                                 <p class="contact-info">casrovers@gmail.com</p>
                             </div>
                             <div class="location">
                                 <div class="image-wrapper">
-                                    <img class="contact-img" src="{{ mix('assets/media/images/icons/geo-alt.svg')  }}" alt="Telephone icon">
+                                    <img class="contact-img" src="{{ mix('assets/media/images/icons/geo-alt.svg')  }}"
+                                         alt="Telephone icon">
                                 </div>
                                 <p class="contact-info">
-                                    House no: Zaalheuvelweg 7, Place: Velp/Grave, Postal code: 5363TE, Country: Netherlands
+                                    House no: Zaalheuvelweg 7, Place: Velp/Grave, Postal code: 5363TE, Country:
+                                    Netherlands
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-1">
+                <div class="col-md-2">
 
                 </div>
                 <div class="col-md-4">
@@ -51,15 +54,39 @@
                             </div>
                             <div class="form-group">
                                 <label class="d-none" for="email"></label>
-                                <input type="email" id="email" class="input form-control" name="email" placeholder="E-mail.">
+                                <input type="email" id="email" class="input form-control" name="email"
+                                       placeholder="E-mail.">
                             </div>
                             <div class="form-group">
                                 <label class="d-none" for="message"></label>
-                                <textarea type="text" id="message" class="input form-control" name="message" placeholder="Message."></textarea>
+                                <textarea type="text" id="message" class="input form-control" name="message"
+                                          placeholder="Message."></textarea>
                             </div>
 
                             <button type="submit" class="contact-submit-btn btn btn-primary">Send</button>
                         </form>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <hr>
+                    <div class="copyright">
+                        <div class="logo">
+                            <a class="logo-link text-light" href="{{ route('home.page') }}">C<span>.</span>Rovers</a>
+                        </div>
+                        <div class="copyright-text d-flex align-items-center">
+                            <p>&copy; Cas Rovers - All rights reserved</p>
+                        </div>
+                        <div class="copyright-btns">
+                            <a href="https://www.facebook.com/" class="info-buttons" target="_blank">
+                                <img src="{{ mix('assets/media/images/icons/facebook.svg') }}" alt="Facebook Logo">
+                            </a>
+                            <a href="https://www.instagram.com/" class="info-buttons" target="_blank">
+                                <img src="{{ mix('assets/media/images/icons/instagram.svg') }}" alt="Facebook Logo">
+                            </a>
+                            <a href="https://github.com/KW1C-Cas-Rovers" class="info-buttons" target="_blank">
+                                <img src="{{ mix('assets/media/images/icons/github.svg') }}" alt="Facebook Logo">
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
