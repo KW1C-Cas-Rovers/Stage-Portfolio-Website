f96a157cb3b5975ec11d1dbe6ddbfd1fd15e66a2
Cas-Rovers <casrovers@outlook.com>
2024-02-03 21:29:58 +0100

    Updated prof experiences page

---
+++ resources/views/partials/experiences/prof-experiences.blade.php
@@ -0,0 +1,26 @@
+<div class="prof-experiences-box">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="prof-experiences-wrapper">
+                    <div class="prof-experience">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="prof-experience-content">
+                                    <h2 class="prof-experience-title">Title</h2>
+                                    <h3 class="prof-experience-subtitle">Subtitle</h3>
+                                    <p class="prof-experience-description">Hello</p>
+                                </div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="prof-experience-image">
+{{--                                     <img src="{{ mix('assets/media/images/Emixion-Logo.png') }}" alt="Emixion Logo"> --}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
