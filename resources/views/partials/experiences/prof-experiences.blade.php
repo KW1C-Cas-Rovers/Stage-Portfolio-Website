39f701d22729f0b866c72dd541d6897e4d388b84
Cas-Rovers <casrovers@outlook.com>
2024-02-04 20:37:22 +0100

    Added professional experiences to the experiences page

--- resources/views/partials/experiences/prof-experiences.blade.php
+++ resources/views/partials/experiences/prof-experiences.blade.php
@@ -7,14 +7,21 @@
                         <div class="row">
                             <div class="col-md-7">
                                 <div class="prof-experience-content">
-                                    <h2 class="prof-experience-title">Title</h2>
-                                    <h3 class="prof-experience-subtitle">Subtitle</h3>
-                                    <p class="prof-experience-description">Hello</p>
+                                    <h2 class="prof-experience-title">{{ __('Stage bij Emixion.') }}</h2>
+                                    <h3 class="prof-experience-subtitle">{{ __('Front-end.') }}</h3>
+                                    <p class="prof-experience-description">
+                                        {{ __('Mijn stage bij Emixion was een waardevolle
+                                               ervaring. Ik heb veel geleerd over front-end
+                                               development en heb een positieve bijdrage
+                                               kunnen leveren aan de projecten waaraan ik
+                                               heb meegewerkt') }}
+                                    </p>
+                                    <span class="date">{{ __('2023 - 2024') }}</span>
                                 </div>
                             </div>
                             <div class="col-md-5">
                                 <div class="prof-experience-image">
-{{--                                     <img src="{{ mix('assets/media/images/Emixion-Logo.png') }}" alt="Emixion Logo"> --}}
+                                    <img class="prof-experience-logo" src="{{ mix('assets/media/images/Emixion-Logo.png') }}" alt="Emixion Logo">
                                 </div>
                             </div>
                         </div>
